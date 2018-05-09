18ea0c57d74597c77d388a76f378fe815a552fac
Jim Jenkins <jjenkins@libertyglobal.com>
2018-05-09 09:48:43 +0200

    no message

--- MAIN.R
+++ MAIN.R
@@ -1,3 +1,7 @@
+
+# Set libraries -----------------------------------------------------------
+
+
 options(scipen=999, digits=8)
 
 library(dlm)
@@ -21,4 +25,27 @@ library(openxlsx)
 source("helper_functions.R")
 source("DLMModel.R")
 
-#-----------------------------------------------------------------------------------------------
+# Read in data ------------------------------------------------------------
+
+ds.prep <- read.csv("./Data/Model_Database_20180504_Until KW201752_Renamed_Columns.csv")
+datadict <- read.csv("./Data/datadict_v2.csv") 
+
+ds.prep$UM_AllChan_Brand_All_SpendGross_2 <- (
+    ds.prep$UM_AllChan_Brand_Internet_SpendGross_2+
+    ds.prep$UM_AllChan_Brand_Magazines_SpendGross_2+
+    ds.prep$UM_AllChan_Brand_Newspapers_SpendGross_2+
+    ds.prep$UM_AllChan_Brand_OOH_SpendGross_2+
+    ds.prep$UM_AllChan_Brand_Radio_SpendGross_2+
+    ds.prep$UM_AllChan_Brand_TradePress_SpendGross_2+
+    ds.prep$UM_AllChan_Brand_TV_SpendGross_2
+)
+
+ds.prep$UM_AllChan_HSIn_All_SpendGross_2 <- (
+    ds.prep$UM_AllChan_HSIn_Internet_SpendGross_2+
+    ds.prep$UM_AllChan_HSIn_Magazines_SpendGross_2+
+    ds.prep$UM_AllChan_HSIn_Newspapers_SpendGross_2+
+    ds.prep$UM_AllChan_HSIn_OOH_SpendGross_2+
+    ds.prep$UM_AllChan_HSIn_Radio_SpendGross_2+
+    ds.prep$UM_AllChan_HSIn_TradePress_SpendGross_2+
+    ds.prep$UM_AllChan_HSIn_TV_SpendGross_2
+)
